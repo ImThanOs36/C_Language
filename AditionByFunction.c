9cc7e6343ec88158c62017f88799c104553ad2d1
ThanOs <lads42279@gmail.com>
2023-02-12 22:44:24 +0530

    Dont know what happen

--- AditionByFunction.c
+++ AditionByFunction.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
-
-
 int sum(int a, int b);
-int main(){
- int a,b;
- printf("Enter first Number : ");
- scanf("%d",&a);
- printf("Enter second Number : ");
- scanf("%d",&b);
+int main()
+{
+    int a, b;
+    printf("Enter first Number : ");
+    scanf("%d", &a);
+    printf("Enter second Number : ");
+    scanf("%d", &b);
 
-int s = sum(a,b);
-printf("sum is %d",s);   
-return 0;
-}  int sum (int a, int b){
+    int s = sum(a, b);
+    printf("sum is %d", s);
+    return 0;
+}
+int sum(int a, int b)
+{
 
-    return a+b;
+    return a + b;
 }
--- AreaOfCirTranRect.c
+++ AreaOfCirTranRect.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 float CircleArea(float redius);
-float TrangleArea(float base ,float height);
+float TrangleArea(float base, float height);
 float RectangleArea(float length, float width);
 
 float main()
@@ -11,17 +11,13 @@ float main()
     scanf("%f", &redius);
     printf("%f \n", CircleArea(redius));
 
-
-
     float base;
     float height;
     printf("Enter Base for Trangle Area :");
     scanf("%f", &base);
     printf("Enter height for Trangle Area :");
     scanf("%f", &height);
-    printf("%f\n", TrangleArea(base,height));
-
-
+    printf("%f\n", TrangleArea(base, height));
 
     float length;
     float width;
@@ -29,24 +25,19 @@ float main()
     scanf("%f", &length);
     printf("Enter width for Trangle Area :");
     scanf("%f", &width);
-    printf("%f\n", RectangleArea(length,width));
+    printf("%f\n", RectangleArea(length, width));
 
-
-
-    
-    
     return 0;
-
 }
 float CircleArea(float redius)
 {
     return 3.14 * redius * redius;
 }
-float TrangleArea(float base,float height)
+float TrangleArea(float base, float height)
 {
     return base * height / 2;
 }
-float RectangleArea(float base,float height)
+float RectangleArea(float base, float height)
 {
-    return base * height ;
+    return base * height;
 }
